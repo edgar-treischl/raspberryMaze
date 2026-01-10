ef21a2402b7fb7182641084bba096d9a2d466c58
edgar.treischl <edgar.treischl@isb.bayern.de>
2026-01-10 14:31:00 +0100

    piezo buzzer 500 hz

--- test_buzzer.py
+++ test_buzzer.py
@@ -1,19 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-BUZZER_PIN = 27  # GPIO-Pin an S-Pin des Buzzer-Moduls
+BUZZER_PIN = 17  # GPIO-Pin an S-Pin des Buzzer-Moduls
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
-try:
-    print("Buzzer AN")
-    GPIO.output(BUZZER_PIN, GPIO.HIGH)
-    time.sleep(2)
 
-    print("Buzzer AUS")
-    GPIO.output(BUZZER_PIN, GPIO.LOW)
+pwm = GPIO.PWM(BUZZER_PIN, 500)
+
+try:
+    pwm.start(90)
     time.sleep(2)
+    pwm.stop()
 
 finally:
     GPIO.cleanup()
